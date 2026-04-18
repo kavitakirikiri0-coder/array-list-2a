2940b7cdab032b79957c0b0ec68bf13c57f5df2c
kavita kirikiri <kavitakirikiri0@gmail.com>
2026-04-18 18:31:17 +0530

    Add files via upload

---
+++ CountryButtonDemo.java
@@ -0,0 +1,50 @@
+package swings;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class CountryButtonDemo {
+
+	
+	
+
+	    public static void main(String[] args) {
+
+	        // Create Frame
+	        JFrame frame = new JFrame("Country Button Demo");
+	        frame.setLayout(new FlowLayout());
+	        frame.setSize(300, 150);
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	        // Create Label
+	        JLabel label = new JLabel("Press a button");
+
+	        // Create Buttons
+	        JButton indiaBtn = new JButton("India");
+	        JButton srilankaBtn = new JButton("Srilanka");
+
+	        // Add ActionListener for India Button
+	        indiaBtn.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+	                label.setText("India is pressed");
+	            }
+	        });
+
+	        // Add ActionListener for Srilanka Button
+	        srilankaBtn.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+	                label.setText("Srilanka is pressed");
+	            }
+	        });
+
+	        // Add components to frame
+	        frame.add(indiaBtn);
+	        frame.add(srilankaBtn);
+	        frame.add(label);
+
+	        // Make visible
+	        frame.setVisible(true);
+	    }
+	}
+
